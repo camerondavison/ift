dc5e9ce4dad63f462353c62308bf7d9c3fd7b1d1
Cameron Davison <cameron@novilabs.com>
2018-12-16 20:44:15 -0600

    adding clap to evaluate a template string

--- src/ift-cli/main.rs
+++ src/ift-cli/main.rs
@@ -1,5 +1,24 @@
+use clap::{crate_authors, crate_version, App, AppSettings, SubCommand};
 use ift::eval;
 
 fn main() {
-    println!("{:?}", eval("en0".to_owned()));
+    let matches = App::new("ift")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .version(crate_version!())
+        .author(crate_authors!())
+        .subcommand(
+            SubCommand::with_name("eval")
+                .about("Evaluate an ift template")
+                .args_from_usage("<template> 'Template string to evaluate'"),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("eval", Some(eval_matches)) => {
+            let template = eval_matches.value_of("template").unwrap();
+            eprintln!("template [{}]", template);
+            println!("{:?}", eval(template));
+        }
+        _ => unreachable!(),
+    }
 }
--- src/ift/lib.rs
+++ src/ift/lib.rs
@@ -1,11 +1,11 @@
 use pnet::datalink::{self, NetworkInterface};
 use std::net::IpAddr;
 
-pub fn eval(s: String) -> Vec<IpAddr> {
+pub fn eval(s: &str) -> Vec<IpAddr> {
     return read_iface(s);
 }
 
-fn read_iface(interface_name: String) -> Vec<IpAddr> {
+fn read_iface(interface_name: &str) -> Vec<IpAddr> {
     let interface_names_match = |iface: &NetworkInterface| iface.name == interface_name;
     let interfaces = datalink::interfaces();
     let interface = interfaces
