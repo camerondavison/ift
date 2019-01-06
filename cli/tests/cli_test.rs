0cb2f23afef97628d42aa58b21885858e71668e9
Cameron Davison <cameron.davison@gmail.com>
2019-01-06 15:45:43 -0600

    os condition in test

--- cli/tests/cli_test.rs
+++ cli/tests/cli_test.rs
@@ -6,16 +6,23 @@ mod tests {
 
     #[test]
     fn all() {
-        let cmds = vec![
-            (
-                ["eval", "GetInterfaceIP \"lo\" | FilterIPv4"],
+        let cmds = if cfg!(target_os = "macos") {
+            vec![
+                (
+                    ["eval", "GetInterfaceIP \"lo1\" | FilterIPv4"],
+                    "[127.0.0.1]\n",
+                ),
+                (
+                    ["eval", "GetInterfaceIP \"lo1\" | FilterIPv6 | FilterFirst"],
+                    "[::1]\n",
+                ),
+            ]
+        } else {
+            vec![(
+                ["eval", "GetInterfaceIP \"lo1\" | FilterIPv4"],
                 "[127.0.0.1]\n",
-            ),
-            (
-                ["eval", "GetInterfaceIP \"lo\" | FilterIPv6 | FilterFirst"],
-                "[::1]\n",
-            ),
-        ];
+            )]
+        };
         for (cmd, stdout) in cmds {
             Command::main_binary()
                 .unwrap()
