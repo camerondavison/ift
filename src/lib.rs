c4c6fa59920d4f4468a0dbd79eba97fcbf066f5d
Cameron Davison <cameron.davison@gmail.com>
2019-01-15 22:01:18 -0600

    more readme docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,52 @@
 #![deny(missing_docs)]
 //! # IFT (interface templates)
 //!
-//! Template strings to extract the correct interface and IpAddr to bind your to
-//!
+//! Template strings to extract the correct interface and IpAddr to bind your to.
 //! Heavily inspired by https://github.com/hashicorp/go-sockaddr
 //!
-//! Most of the time you are going to want to use the [eval](https://camerondavison.github.io/ift/ift/fn.eval.html#evaluate-a-interface-template)
-//! function to evaluate a template.
+//! ## What is it?
+//! `eval`([docs](https://camerondavison.github.io/ift/ift/fn.eval.html#evaluate-a-interface-template))
+//! takes an interface template string. The template is a string that starts with a
+//! [producer](https://camerondavison.github.io/ift/ift/fn.eval.html#producers)
+//! and is followed by [filters](https://camerondavison.github.io/ift/ift/fn.eval.html#filters)
+//! and [sorts](https://camerondavison.github.io/ift/ift/fn.eval.html#sorts)
+//! each of which is pipe `|` delimited. `eval` returns a vector of [IpAddr](https://doc.rust-lang.org/std/net/enum.IpAddr.html) objects
+//! that can then be used as bindings
 //!
-//! The template is a string that starts with a [producer](https://camerondavison.github.io/ift/ift/fn.eval.html#producers)
-//! and is followed by [filters](https://camerondavison.github.io/ift/ift/fn.eval.html#filters) and [sorts](https://camerondavison.github.io/ift/ift/fn.eval.html#sorts)
-//! each of which is pipe `|` delimited. `eval` returns a vector of IpAddr objects
-//! that can then be used to bind to.
+//! ## Usage
 //!
-//! Sometimes it makes more sense to bind to a single address. The [evals](https://camerondavison.github.io/ift/ift/fn.evals.html)
-//! function helps with this, by expecting everything will go smoothly and returning
-//! and `Optional<IpAddr>`
+//! ### general
+//! ```
+//! use ift::eval;
+//! print!("{:?}", eval(r#"GetInterface "en0""#).unwrap());
+//! ```
+//!
+//! ### actix
+//! ```
+//! use actix_web::{
+//!    server,
+//!    App,
+//! };
+//! let mut s = server::new(|| { App::new() });
+//! for ip in ift::eval("GetPrivateInterfaces").unwrap().into_iter() {
+//!   s = s.bind((ip, 8080)).unwrap();
+//! }
+//! ```
+//!
+//! ### Example Templates
+//! - get private interfaces
+//!   `GetAllInterfaces | FilterFlags "up" | FilterForwardable | SortBy "default"`
+//! - get private interfaces short
+//!   `GetPrivateInterfaces`
+//! - get specific interface by name
+//!   `GetInterface "en0"`
+//! - get only interfaces with ipv6 addresses
+//!   `GetAllInterfaces | FilterIPv6`
+//!
+//! ## Examples
+//! There are examples in the [examples](https://github.com/camerondavison/ift/tree/master/examples)
+//! folder.
+//! * [actix](https://github.com/camerondavison/ift/blob/master/examples/actix.rs) - bind multiple private interfaces
 //!
 use failure::{
     Error,
